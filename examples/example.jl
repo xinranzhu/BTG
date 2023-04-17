01e1ce32ecd7723512ae220eddd1693e1c9c7ba9
Xinran Zhu <xz584@cornell.edu>
2023-04-17 16:27:19 -0400

    clean up example

--- examples/example.jl
+++ examples/example.jl
@@ -1,11 +1,6 @@
 include("../src/BTG.jl")
 ntrain = 10
 trainingdata0 = get_int_sine_data(; ntrain=ntrain)
-xtrain = getposition(trainingdata0)
-ytrain = getlabel(trainingdata0)
-dimx = getdimension(trainingdata0)
-feature_size = 1
-@assert dimx == feature_size
 
 # multi-point prediction
 ntest = 400
